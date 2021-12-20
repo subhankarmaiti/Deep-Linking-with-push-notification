4e589ea0b00f8d15752b016049fe0743572df2cf
Subhankar Maiti <subhankarmaiti40@gmail.com>
2021-12-20 23:12:24 +0530

    deeplinking with react navigation

--- expo-app/src/navigation/index.js
+++ expo-app/src/navigation/index.js
@@ -1,10 +1,40 @@
+import * as Linking from 'expo-linking';
+
+import React, {useEffect, useState} from 'react';
+
 import DrawerNavigation from './DrawerNavigation';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+
+const prefix = Linking.createURL('/');
 
 export default function Navigation() {
+    const [data, setData] = useState(null);
+    const linking = {
+        prefixes: [prefix],
+    };
+    useEffect(() =>{
+        const getInitialUrl = async () => {
+            const initialUrl = await Linking.getInitialURL();
+            console.log(initialUrl);
+            if(initialUrl) {
+                setData(Linking.parse(initialUrl));
+            }
+        };
+        // open the app when in the background
+        Linking.addEventListener('url', handleDeepLink);
+
+        if(!data) {
+            getInitialUrl();
+        }
+
+        return Linking.removeEventListener('url');
+    },[]);
+    const handleDeepLink = event => {
+        let data = Linking.parse(event.url);
+        setData(data);
+    };
     return (
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
             <DrawerNavigation />
         </NavigationContainer>
     );
